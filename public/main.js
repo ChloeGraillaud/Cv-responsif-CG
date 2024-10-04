37a06876495f4c292194e7d50d4c1f8223469192
ChloeGraillaud <cgraillaud@laposte.net>
2024-10-04 16:43:03 +0200

    Fix translation

--- public/main.js
+++ public/main.js
@@ -291,6 +291,7 @@ function loadTranslations(lang) {
       document.getElementById('card6').innerHTML = data[lang].card6;
       document.getElementById('card7').innerHTML = data[lang].card7;
       document.getElementById('card8').innerHTML = data[lang].card8;
+      document.getElementById('titlePortfolio3').innerHTML = data[lang].titlePortfolio3;
       document.getElementById('card9').innerHTML = data[lang].card9;
 
       //Ma Formation
