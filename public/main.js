044446704d5e66400715c37f20a687d691998aa3
ChloeGraillaud <cgraillaud@laposte.net>
2025-06-01 14:33:49 +0200

    Update cv

--- public/main.js
+++ public/main.js
@@ -293,6 +293,7 @@ function loadTranslations(lang) {
       document.getElementById('card8').innerHTML = data[lang].card8;
       document.getElementById('titlePortfolio3').innerHTML = data[lang].titlePortfolio3;
       document.getElementById('card9').innerHTML = data[lang].card9;
+      document.getElementById('card10').innerHTML = data[lang].card10;
 
       //Ma Formation
       document.getElementById('titleFormation').innerHTML = data[lang].titleFormation;
